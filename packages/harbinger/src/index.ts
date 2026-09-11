7e2e438d9dc0eff13ffd844eda290eb948eba341
kelvinpraises <kelvinpraises@gmail.com>
2026-09-11 17:17:36 +0100

    feat(harbinger): export reflex loop and journal services from package entrypoints

--- packages/harbinger/src/index.ts
+++ packages/harbinger/src/index.ts
@@ -2,6 +2,7 @@ export * from "./config";
 export * from "./daemon";
 export * from "./models/index";
 export * from "./infrastructure/database/index";
+export * from "./infrastructure/cron/index";
 export * from "./services/index";
 export * from "./types";
 export * from "./tools/index";
--- packages/harbinger/src/services/index.ts
+++ packages/harbinger/src/services/index.ts
@@ -1,4 +1,6 @@
 export * from "./graph/index";
 export * from "./chain/index";
 export * from "./tension-cast/index";
+export * from "./journal/index";
+export * from "./loop/index";
 export * from "./oracle/index";
